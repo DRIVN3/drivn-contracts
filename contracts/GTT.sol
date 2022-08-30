6668602115674b5e71ffa473963498b9bf78edd3
3NFBagdu <gbagh16@freeuni.edu.ge>
2022-08-30 14:48:00 +0400

    feat: GTT token and burn wallet implemented

---
+++ contracts/GTT.sol
@@ -0,0 +1,140 @@
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+interface IGTT {
+
+    /**
+     * @dev burns coins on burn wallet ballance
+     * @param count count of coins
+    */
+
+    function burn(uint256 count) external;
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+}
+
+contract GTTBurnWallet is Ownable {
+    
+    // GTT token
+    IGTT immutable token;
+
+    /**
+        * @dev Constructing the GTTBurnWallet contract
+        * @param owner_ address of owner
+    */
+
+    constructor(
+        address GTT_,
+        address owner_
+    )
+    {
+        _transferOwnership(owner_);
+        token = IGTT(GTT_);
+    }
+
+    /**
+     * @dev burning GTT coins
+    */
+
+    function burn() external onlyOwner {
+        uint256 toBurn = token.balanceOf(address(this));
+        token.burn(toBurn);
+    }
+}
+
+contract GTT is ERC20, Ownable, Pausable {
+
+    // burn wallet contract
+    GTTBurnWallet public burnWallet;
+
+    // start coins
+    uint256 public constant startCoins = 200000 * 10**18;
+
+    /**
+     * @dev Constructing the contract minting 200000 coin to the contract address and setting name, symbol
+    */
+
+    constructor(
+        string memory name_, 
+        string memory symbol_
+    )
+    ERC20(name_, symbol_)
+    {
+
+        // minting starting coins
+        _mint(address(this), startCoins);
+
+        burnWallet = new GTTBurnWallet(address(this), owner());
+
+    }
+
+    /**
+     * @dev modifier to detect the caller is the burnWallet address
+    */
+
+    modifier onlyBurnWallet() {
+        require(msg.sender == address(burnWallet), "GTT: address does not have burn access");
+        _;
+    }
+
+    /**
+     * @dev burns coins on burn wallet ballance
+     * @param count count of coins
+    */
+
+    function burn(uint256 count) external onlyBurnWallet {
+        _burn(msg.sender, count);
+    }
+
+    /**
+     * @dev airdrop the coins to accounts
+     * @param accounts array of accounts
+     * @param counts array of counts of coins
+    */
+
+    function airdrop(address[] calldata accounts, uint256[] calldata counts) external onlyOwner {
+        require(accounts.length == counts.length, "GTT: invalid Data lengths mismatch");
+
+        for (uint256 i = 0; i < accounts.length; ++i) {
+            require(accounts[i] != address(0), "GTT: zero address included");
+            _transfer(address(this), accounts[i], counts[i]);
+        }
+    }
+
+    /**
+     * @dev pausing the contract, where transfers or minting will be retricted
+    */
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    /**
+     * @dev unpausing the contract, where transfers or minting will be possible
+    */
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    /**
+     * @dev overriding before token transfer from ERC20 contract, adding whenNotPaused modifier to restrict transfers while paused.
+    */
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+    internal
+    whenNotPaused
+    override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
---
+++ test/GTT.js
@@ -0,0 +1,113 @@
+const {
+    time,
+    loadFixture,
+    constants,
+  } = require("@nomicfoundation/hardhat-network-helpers");
+  const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+  const { expect } = require("chai");
+let bigInt = require("big-integer");
+
+const startGTTCoins = bigInt("200000000000000000000000");
+  
+async function deployGTT() {
+    const [owner, firstAccount, secondAccount] = await ethers.getSigners();
+
+    // deploy GTT
+    let GTT = await ethers.getContractFactory("GTT");
+    let name = "test";
+    let symbol = "testing";
+    GTT = await GTT.deploy(name, symbol);
+
+    // attach burn wallet 
+    let burnWallet = await ethers.getContractFactory("GTTBurnWallet");
+    burnWallet = await burnWallet.attach(await GTT.burnWallet());
+
+    return { GTT, name, symbol, owner, firstAccount, secondAccount, burnWallet};
+}
+
+
+describe("Deployment", function () {
+    it("Checking initial balance", async function () {
+        const { GTT, name, symbol, owner } = await loadFixture(deployGTT);
+        expect(await GTT.balanceOf(GTT.address)).to.equal(startGTTCoins.toString());
+        expect(await GTT.name()).to.be.equal(name);
+        expect(await GTT.symbol()).to.be.equal(symbol);
+        expect(await GTT.owner()).to.be.equal(owner.address);
+    });
+});
+
+
+describe("Test Airdrop", function () {
+    
+    it("Should fail while calling not owner", async function () {
+        const { GTT, owner, firstAccount } = await loadFixture(deployGTT);
+        await expect(GTT.connect(firstAccount).airdrop([owner.address], [1])).to.be.revertedWith("Ownable: caller is not the owner")
+    });
+
+    it("Should fail when array length is different", async function () {
+        const { GTT, owner, firstAccount } = await loadFixture(deployGTT);
+        await expect(GTT.airdrop([owner.address], [1, 2])).to.be.revertedWith("GTT: invalid Data lengths mismatch")
+        await expect(GTT.airdrop([owner.address, firstAccount.address], [1])).to.be.revertedWith("GTT: invalid Data lengths mismatch")
+    });
+
+    it("Should fail when giving zero address", async function () {
+        const { GTT } = await loadFixture(deployGTT);
+        await expect(GTT.airdrop([ethers.constants.AddressZero], [1])).to.be.revertedWith("GTT: zero address included")
+    });
+
+    it("Should address get tokens after airdrop calls", async function () {
+        const { GTT, firstAccount, secondAccount } = await loadFixture(deployGTT);
+
+        // airdroping two different accoutns
+        await GTT.airdrop([firstAccount.address, secondAccount.address], [10, 150])
+
+        // checking balances
+        expect(await GTT.balanceOf(firstAccount.address)).to.be.equal(10)
+        expect(await GTT.balanceOf(secondAccount.address)).to.be.equal(150)
+    });
+    
+});
+
+
+describe("Test pause", function () {
+    it("Should revert airdrop when paused", async function () {
+        const { GTT, firstAccount } = await loadFixture(deployGTT);
+        // test pause
+        await GTT.pause();
+        await expect(GTT.airdrop([firstAccount.address], [2])).to.be.revertedWith("Pausable: paused");        
+    });
+
+    it("Should fail transfer when paused", async function () {
+        const { GTT, firstAccount } = await loadFixture(deployGTT);
+        // test pause
+        await GTT.pause();
+        await expect(GTT.transfer(firstAccount.address, 2)).to.be.revertedWith("Pausable: paused");        
+    });
+
+    it("Should revert airdrop when paused", async function () {
+        const { GTT, firstAccount } = await loadFixture(deployGTT);
+        await GTT.pause();        
+        await GTT.unpause();
+        await GTT.airdrop([firstAccount.address], [2])
+        expect(await GTT.balanceOf(firstAccount.address)).to.equal(2);
+    });
+
+});
+  
+describe("Test Burn", function () {
+    it("Should revert if the caller is not a burn GTT wallet", async function () {
+        const { GTT } = await loadFixture(deployGTT);
+        await expect(GTT.burn(100)).to.be.revertedWith("GTT: address does not have burn access");        
+    });
+
+    it("Should burn proper amount of coins", async function () {
+        const { GTT, burnWallet, owner } = await loadFixture(deployGTT);
+        
+        await GTT.airdrop([owner.address], [200]);
+        await GTT.transfer(burnWallet.address, 150);
+        await burnWallet.burn()
+
+        expect(await GTT.balanceOf(burnWallet.address)).to.be.equal(0)
+        expect(await GTT.totalSupply()).to.be.equal(startGTTCoins.minus(150).toString())
+    });
+});
