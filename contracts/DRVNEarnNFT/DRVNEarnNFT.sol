ec3e441a822322b73c8061250e3ff5ebf3db86d7
3NFBagdu <gbagh16@freeuni.edu.ge>
2022-09-01 15:58:30 +0400

    feat: add merge + minting

---
+++ contracts/DRVNEarnNFT/DRVNEarnNFT.sol
@@ -0,0 +1,159 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract EarnNFT is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    // token counter
+    Counters.Counter private _tokenIdCounter;
+
+    // enum for electic vehicle
+    enum EVehicle { CAR, BYCICLE, SCOOTER }
+
+    // enum for NFT type
+    enum Type { COMMON, UNCOMMON, RARE, EPIC }
+
+    // struct NFT information
+    struct NFTInformation {
+        Type nftType;
+        uint256 lastUsage;
+        uint256 powerLeft;
+        uint256 maxPower;
+    }
+
+    // mapping for nft information
+    mapping(uint256=>NFTInformation) public nftInfo;
+
+    // mapping for nft information
+    mapping(Type=>uint256) public nftTypePower;
+
+    // max possible supply
+    uint256 public constant maxSupply = 10000;
+
+    // base token URI
+    string internal _baseTokenURI;
+
+    // commong token price
+    uint256 public commonTokenPrice = 0.01 ether;
+
+    /**
+     * @dev Sets main dependencies and constants
+     * @param name_ 721A nft name
+     * @param symbol_ 721A nft symbol
+     * @param baseURI baseUri for mint
+    */
+
+    constructor(string memory name_, string memory symbol_, string memory baseURI) 
+    ERC721(name_, symbol_){
+        setBaseURI(baseURI);
+
+        // define powers
+        nftTypePower[Type.COMMON] = 1 * powerMultiplier();
+        nftTypePower[Type.UNCOMMON] = 2 * powerMultiplier();
+        nftTypePower[Type.RARE] = 3 * powerMultiplier();
+        nftTypePower[Type.EPIC] = 4 * powerMultiplier();
+    }
+
+
+    /**
+     * @dev buying the token
+    */
+
+    function mint() external payable {
+        require(commonTokenPrice == msg.value, "EarnNFT: not enough money");
+
+        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
+        _mint(msg.sender, tokenId);
+
+        nftInfo[tokenId] = NFTInformation(
+            Type.COMMON, // nft type is common
+            0, // last usage
+            nftTypePower[Type.COMMON], // powerLeft
+            nftTypePower[Type.COMMON] // max power
+        );
+    }
+
+
+    /**
+     * @dev merging two nft
+     * @param tokenId1 first nft id for merging
+     * @param tokenId2 second nft id for merging
+    */
+
+    function merge(uint256 tokenId1, uint256 tokenId2) external {
+        require(ownerOf(tokenId1) == msg.sender 
+                    && ownerOf(tokenId2) == msg.sender, 
+                    "EarnNFT: sender is not the owner of the tokens");
+        
+        uint256 newPower = nftInfo[tokenId1].maxPower + nftInfo[tokenId2].maxPower;
+        Type nftType = getTypeByPower(newPower);    
+
+        // adding the token
+        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
+        _mint(msg.sender, tokenId);
+
+        nftInfo[tokenId] = NFTInformation(
+            nftType, // nft type is common
+            0, // last usage
+            newPower, // powerLeft
+            newPower // maxPower
+        );
+
+        // burning mergin tokens
+        delete nftInfo[tokenId1];
+        delete nftInfo[tokenId2];
+        _burn(tokenId1);
+        _burn(tokenId2);
+    }
+
+
+
+    /**
+     * @dev Set the base URI
+     * @param baseURI_ Base path to metadata
+    */
+
+    function setBaseURI(string memory baseURI_) public onlyOwner {
+        _baseTokenURI = baseURI_;
+    }
+
+    /**
+     * @dev Get current base uri 
+    */
+
+    function _baseURI() internal view override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    /**
+        @dev pure function for returning decimals of power
+    */
+
+    function powerMultiplier() public pure returns (uint256) {
+        return 3600;
+    }
+
+    /**
+        @dev pure function for returning type by power
+    */
+
+    function getTypeByPower(uint256 power) public pure returns (Type) {
+        require(power <= 4 * powerMultiplier(), "EarnNFT: Merge Power is too high");
+        if (power == 1 * powerMultiplier())
+            return Type.COMMON;  
+        if (power == 2 * powerMultiplier()) 
+            return Type.UNCOMMON;
+        if (power == 3 * powerMultiplier())
+            return Type.RARE;
+        if (power == 4 * powerMultiplier())
+            return Type.EPIC;
+    }
+
+}
---
+++ test/EarnNFT.js
@@ -0,0 +1,184 @@
+const { expect } = require("chai");
+const { ethers, network, upgrades } = require("hardhat");
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+
+const utils = ethers.utils;
+let bigInt = require("big-integer");
+
+const COMMON = 0, UNCOMMON = 1, RARE = 2, EPIC = 3;
+const COMMONPOWER = 1, UNCOMMONPOWER = 2, RAREPOWER = 3, EPICPOWER = 4;
+
+async function getContracts() {
+    const [owner, firstAccount, secondAccount] = await ethers.getSigners();
+
+    let earnNFT = await ethers.getContractFactory("EarnNFT"); 
+    let name = "test";
+    let symbol = "testing";  
+    let baseUri = "testing";
+    earnNFT = await earnNFT.deploy(name, symbol, baseUri); 
+
+    return { earnNFT, owner, firstAccount, secondAccount, name, symbol, baseUri } 
+}
+
+
+describe("EarnNFt", function () { 
+    describe("EarnNFT deployment", function () {
+        describe("Deployment", function () {
+            it("Checking the contracts", async function () {
+                const { earnNFT, name, symbol, baseUri, owner } = await loadFixture(getContracts);
+                expect(await earnNFT.name()).to.be.equal(name);
+                expect(await earnNFT.symbol()).to.be.equal(symbol);
+                expect(await earnNFT.owner()).to.be.equal(owner.address);
+            });
+
+            it("Checking power decimals", async function () {
+                const { earnNFT } = await loadFixture(getContracts);
+                expect(await earnNFT.powerMultiplier()).to.be.equal(3600);
+            });
+
+            it("Checking getTypeByPower", async function () {
+                const { earnNFT } = await loadFixture(getContracts);
+                expect(await earnNFT.getTypeByPower(await earnNFT.powerMultiplier())).to.be.equal(COMMON);
+            });
+        });
+    });
+
+    describe("test EarnNft minting", function () {
+        it("Should fail when not enough balance", async function () {
+            const { earnNFT, firstAccount } = await loadFixture(getContracts);
+            await expect(earnNFT.connect(firstAccount).mint()).to.be.revertedWith("EarnNFT: not enough money");
+        });
+
+        it("Should mint correctly", async function () {
+            const { earnNFT, firstAccount, baseUri } = await loadFixture(getContracts);
+            await earnNFT.connect(firstAccount).mint({value: ethers.utils.parseEther('0.01')});
+            
+            expect(await earnNFT.ownerOf(1)).to.be.equal(firstAccount.address);
+            expect(await earnNFT.tokenURI(1)).to.be.equal(baseUri+'1');
+        });
+
+        it("Should get correct information after minting", async function () {
+            const { earnNFT, firstAccount, baseUri } = await loadFixture(getContracts);
+            await earnNFT.connect(firstAccount).mint({value: ethers.utils.parseEther('0.01')});
+            
+
+            let multiplier = await earnNFT.powerMultiplier();
+            let nftInfo = await earnNFT.nftInfo(1);
+            expect(nftInfo.nftType).to.be.equal(COMMON);
+            expect(nftInfo.lastUsage).to.be.equal(0);
+            expect(nftInfo.powerLeft).to.be.equal(COMMONPOWER * multiplier);
+            expect(nftInfo.maxPower).to.be.equal(COMMONPOWER * multiplier);
+        });
+    });
+
+    describe("test EarnNft merging", function () {
+        it("Should fail when merge is called with no owner of the tokens", async function () {
+            const { earnNFT, firstAccount, secondAccount } = await loadFixture(getContracts);
+            await earnNFT.connect(firstAccount).mint({value: ethers.utils.parseEther('0.01')});
+            await earnNFT.connect(firstAccount).mint({value: ethers.utils.parseEther('0.01')});
+            await earnNFT.connect(secondAccount).mint({value: ethers.utils.parseEther('0.01')});
+
+            await expect(earnNFT.connect(secondAccount).merge(1, 2)).to.be.revertedWith("EarnNFT: sender is not the owner of the tokens");
+            await expect(earnNFT.connect(secondAccount).merge(3, 2)).to.be.revertedWith("EarnNFT: sender is not the owner of the tokens");
+        });
+
+        it("Should merge two common token", async function () {
+            const { earnNFT, firstAccount, secondAccount } = await loadFixture(getContracts);
+            await earnNFT.connect(firstAccount).mint({value: ethers.utils.parseEther('0.01')});
+            await earnNFT.connect(firstAccount).mint({value: ethers.utils.parseEther('0.01')});
+
+            await earnNFT.connect(firstAccount).merge(1, 2);
+            
+            // the tokens get burned
+            await expect(earnNFT.ownerOf(1)).to.be.revertedWith("ERC721: invalid token ID");
+            await expect(earnNFT.ownerOf(2)).to.be.revertedWith("ERC721: invalid token ID");
+
+            // the merge happens correctly
+            expect(await earnNFT.ownerOf(3)).to.be.equal(firstAccount.address);
+            
+            let multiplier = await earnNFT.powerMultiplier();
+
+            let nftInfo = await earnNFT.nftInfo(3);
+            expect(nftInfo.nftType).to.be.equal(UNCOMMON);
+            expect(nftInfo.lastUsage).to.be.equal(0);
+            expect(nftInfo.powerLeft).to.be.equal(UNCOMMONPOWER * multiplier);
+            expect(nftInfo.maxPower).to.be.equal(UNCOMMONPOWER * multiplier);
+        });
+
+        it("Should get the RARE nft token", async function () {
+            const { earnNFT, firstAccount, secondAccount } = await loadFixture(getContracts);
+            await earnNFT.connect(firstAccount).mint({value: ethers.utils.parseEther('0.01')});
+            await earnNFT.connect(firstAccount).mint({value: ethers.utils.parseEther('0.01')});
+            await earnNFT.connect(firstAccount).mint({value: ethers.utils.parseEther('0.01')});
+
+            await earnNFT.connect(firstAccount).merge(1, 2);
+            await earnNFT.connect(firstAccount).merge(3, 4);
+
+            // the tokens get burned
+            await expect(earnNFT.ownerOf(1)).to.be.revertedWith("ERC721: invalid token ID");
+            await expect(earnNFT.ownerOf(2)).to.be.revertedWith("ERC721: invalid token ID");
+            await expect(earnNFT.ownerOf(3)).to.be.revertedWith("ERC721: invalid token ID");
+            await expect(earnNFT.ownerOf(4)).to.be.revertedWith("ERC721: invalid token ID");
+
+            // the merge happens correctly
+            expect(await earnNFT.ownerOf(5)).to.be.equal(firstAccount.address);
+
+            let multiplier = await earnNFT.powerMultiplier();
+            let nftInfo = await earnNFT.nftInfo(5);
+            expect(nftInfo.nftType).to.be.equal(RARE);
+            expect(nftInfo.lastUsage).to.be.equal(0);
+            expect(nftInfo.powerLeft).to.be.equal(RAREPOWER * multiplier);
+            expect(nftInfo.maxPower).to.be.equal(RAREPOWER * multiplier);
+        });
+
+        it("Should get the EPIC nft token", async function () {
+            const { earnNFT, firstAccount, secondAccount } = await loadFixture(getContracts);
+            await earnNFT.connect(firstAccount).mint({value: ethers.utils.parseEther('0.01')});
+            await earnNFT.connect(firstAccount).mint({value: ethers.utils.parseEther('0.01')});
+            await earnNFT.connect(firstAccount).mint({value: ethers.utils.parseEther('0.01')});
+
+            await earnNFT.connect(firstAccount).merge(1, 2);
+            await earnNFT.connect(firstAccount).merge(3, 4);
+
+            await earnNFT.connect(firstAccount).mint({value: ethers.utils.parseEther('0.01')});
+            await earnNFT.connect(firstAccount).merge(5, 6);
+
+            // the tokens get burned
+            await expect(earnNFT.ownerOf(1)).to.be.revertedWith("ERC721: invalid token ID");
+            await expect(earnNFT.ownerOf(2)).to.be.revertedWith("ERC721: invalid token ID");
+            await expect(earnNFT.ownerOf(3)).to.be.revertedWith("ERC721: invalid token ID");
+            await expect(earnNFT.ownerOf(4)).to.be.revertedWith("ERC721: invalid token ID");
+            await expect(earnNFT.ownerOf(5)).to.be.revertedWith("ERC721: invalid token ID");
+            await expect(earnNFT.ownerOf(6)).to.be.revertedWith("ERC721: invalid token ID");
+
+            // the merge happens correctly
+            expect(await earnNFT.ownerOf(7)).to.be.equal(firstAccount.address);
+
+            let multiplier = await earnNFT.powerMultiplier();
+            let nftInfo = await earnNFT.nftInfo(7);
+            expect(nftInfo.nftType).to.be.equal(EPIC);
+            expect(nftInfo.lastUsage).to.be.equal(0);
+            expect(nftInfo.powerLeft).to.be.equal(EPICPOWER * multiplier);
+            expect(nftInfo.maxPower).to.be.equal(EPICPOWER * multiplier);
+        });
+
+        it("Should fail while merging Rare and UNCOMMON", async function () {
+            const { earnNFT, firstAccount, secondAccount } = await loadFixture(getContracts);
+            await earnNFT.connect(firstAccount).mint({value: ethers.utils.parseEther('0.01')});
+            await earnNFT.connect(firstAccount).mint({value: ethers.utils.parseEther('0.01')});
+            await earnNFT.connect(firstAccount).mint({value: ethers.utils.parseEther('0.01')});
+
+            await earnNFT.connect(firstAccount).merge(1, 2);
+            // RARE in 5 id
+            await earnNFT.connect(firstAccount).merge(3, 4);
+
+            // UNCOMMON 8 ID
+            await earnNFT.connect(firstAccount).mint({value: ethers.utils.parseEther('0.01')});
+            await earnNFT.connect(firstAccount).mint({value: ethers.utils.parseEther('0.01')});
+            await earnNFT.connect(firstAccount).merge(6, 7);
+
+            await expect(earnNFT.connect(firstAccount).merge(5, 8)).to.be.revertedWith("EarnNFT: Merge Power is too high");
+        });
+
+    });
+});
