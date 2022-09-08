461b13a2616029ac76ea949d2b0ebc1a3aa7600c
3NFBagdu <gbagh16@freeuni.edu.ge>
2022-09-08 14:30:11 +0400

    fix: remove openapi

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,30 +6,8 @@ from config import settings
 
 from app.routers import nft_metadata
 
-
-middleware = [
-    # add sentry SDK into here at a later time
-]
-
-
-app = FastAPI(middleware=middleware)
+app = FastAPI()
 app.include_router(nft_metadata.router)
-
-def custom_openapi():
-    if app.openapi_schema:
-        return app.openapi_schema
-    openapi_schema = get_openapi(
-        title="openapi",
-        version="2.5.0",
-        description="openapi",
-        routes=app.routes,
-    )
-    app.openapi_schema = openapi_schema
-    return app.openapi_schema
-
-
-app.openapi = custom_openapi
-
 
 if __name__ == "__main__":
     uvicorn.run(
